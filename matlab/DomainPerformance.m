959b15eb8a0d8518280ec69ff010f93eea1360d3
Dean Shaff <dshaff@swin.edu.au>
2019-07-03 14:53:02 +1000

    working on setting up gitlab pages

--- matlab/DomainPerformance.m
+++ matlab/DomainPerformance.m
@@ -10,9 +10,12 @@ classdef DomainPerformance
     function res = temporal_performance(obj, a)
       err = ErrorAnalysis;
       a = abs(a).^2;
+      % res = [err.max_spurious_power(a),...
+      %        err.total_spurious_power(a),...
+      %        err.mean_spurious_power(a)];
       res = [err.max_spurious_power(a),...
-             err.total_spurious_power(a),...
-             err.mean_spurious_power(a)];
+             err.total_spurious_power(a)];
+
       % diff = a - b;
       % mean_diff = mean(abs(diff));
       % max_diff = max(abs(diff));
@@ -23,9 +26,11 @@ classdef DomainPerformance
     function res = spectral_performance(obj, a, fft_length)
       err = ErrorAnalysis;
       a_fft = abs(fft(a, fft_length)./fft_length).^2;
+      % res = [err.max_spurious_power(a_fft),...
+      %        err.total_spurious_power(a_fft),...
+      %        err.mean_spurious_power(a_fft)];
       res = [err.max_spurious_power(a_fft),...
-             err.total_spurious_power(a_fft),...
-             err.mean_spurious_power(a_fft)];
+             err.total_spurious_power(a_fft)];
     end
   end
 end
