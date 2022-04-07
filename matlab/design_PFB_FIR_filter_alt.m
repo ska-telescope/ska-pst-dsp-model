bba2a9d594c3d717a0b4c890b7138b6b13efe198
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-08 10:47:52 +1200

    AT3-150 incorporate the oversampling ratio into the size of the array passed to fircls1

--- matlab/design_PFB_FIR_filter_alt.m
+++ matlab/design_PFB_FIR_filter_alt.m
@@ -32,7 +32,7 @@ function fir_filter_path = design_PFB_FIR_filter_alt(n_chan, os_factor, n_taps_p
   dBs = -70;
   ds = 10^(dBs/20);    % Maximum stopband deviation from 0 (stopband ripple)
 
-  intermediate_nchan=16;
+  oversampled_ntaps_per_chan = n_taps_per_chan * os_factor.nu / os_factor.de;
 
   % Filter specs for the prototype filter
   method = 1;
@@ -41,7 +41,7 @@ function fir_filter_path = design_PFB_FIR_filter_alt(n_chan, os_factor, n_taps_p
     fprintf ('design_PFB_FIR_filter_alt: optimizing for overlap-save\n');
     % John Bunton recommends this configuration for spectral overlap-save
     % flat to bandedge stopband at 0.8333
-    n  = intermediate_nchan*n_taps_per_chan - 1;
+    n  = oversampled_ntaps_per_chan*n_taps_per_chan - 1;
     wo = 1/n_taps_per_chan;  % Cut-off frequency
     dp = 0.002;   % Maximum passband deviation from 1 (passband ripple)
     %ds = 0.0002;  % Maximum stopband deviation from 0 (stopband ripple)
@@ -97,52 +97,7 @@ function fir_filter_path = design_PFB_FIR_filter_alt(n_chan, os_factor, n_taps_p
       
     fprintf ('plotting response\n');
       
-    [H0,W] = freqz (h, 1, n_taps);
-
-    %Rescaling the frequency axis
-    W = W/pi;
-
-    Fp=wo*intermediate_nchan/n_chan;
-    Fs=OS*Fp;
-    % Filter Transfer Function specs
-    Ap = ds;
-    As = dBs;
-  
-    fprintf('design_PFB: cut-off frequency: %f\n', Fp);
-    fprintf('design_PFB: stop-band frequency: %f\n', Fs);
-
-    fig = figure;
-    subplot(3,1,1)
-    plot (W, abs(H0), 'LineWidth', 1.5);
-    axis ([0 3.5*Fp -0.15 1.15]);
-    title(sprintf('Transfer Function of the Prototype Filter with %d taps', n_taps));
-    grid on; box on;
-
-    subplot(3,1,2)
-    hold on;
-    plot (W, 20*log10(abs(H0)));
-    log_Ap_min = 20*log10(1-Ap);
-    log_Ap_max = 20*log10(1+Ap);
-    plot([0 Fp], [log_Ap_min log_Ap_min],'k-.','LineWidth',1);
-    plot([0 Fp], [log_Ap_max log_Ap_max],'k-.','LineWidth',1);
-    hold off;
-    
-    log_Ap_min = 20*log10(1-5*Ap);
-    log_Ap_max = 20*log10(1+5*Ap);
-
-    % axis ([0 1.5*Fp -1000*Ap 1000*Ap]);
-    xlim([0 0.5*(Fp+Fs)]);
-    title ('Passband')
-    grid on; box on;
-    %
-    subplot (3,1,3);
-    hold on;
-    plot (W, 20*log10(abs(H0)));
-    plot([Fs 1], [As As],'r-','LineWidth',1);
-    hold off;
-    axis ([0.5*Fs 4*Fp (As-10) 3]);
-    title ('Stopband')
-    grid on; box on;
+    fig = plot_FIR_filter (n_chan, OS, h);
 
     saveas(fig, sprintf('./../products/FIR_filter_response.%d.png', n_taps));
 
