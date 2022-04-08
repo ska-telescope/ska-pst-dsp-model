b6273440ca294fe13b6034fe7e27b9836995e7df
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-08 21:57:48 +1200

    AT3-150 output all 256 channels (do not drop 20 from either side)

--- matlab/PSTFilterbank.m
+++ matlab/PSTFilterbank.m
@@ -11,14 +11,14 @@ dinp = zeros(totalSamples+2880,1);
 dinp(2881:end) = din;
 
 %% initialise
-dout = zeros(216,outputSamples);
+dout = zeros(256,outputSamples);
 fftIn = zeros(256,1);
 
 %% 
 for outputSample = 1:outputSamples
     % FIR filter, with scaling
     for n1 = 1:256
-        fftIn(n1) = sum(FIRtaps(n1:256:end) .* dinp((outputSample-1)*192 + (n1:256:(n1+256*11))))/2^9;
+        fftIn(n1) = sum(FIRtaps(n1:256:end-1) .* dinp((outputSample-1)*192 + (n1:256:(n1+256*11))))/2^9;
         
      %   if (outputSample == 10)
      %       disp(FIRtaps(n1:256:end));
@@ -51,6 +51,6 @@ for outputSample = 1:outputSamples
    %     keyboard
    % end    
     
-    dout(:,outputSample) = dout2(21:(21 + 215));
+    dout(:,outputSample) = dout2;
 end
 
