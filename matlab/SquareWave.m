8d3362c8a837abef6e2774a2523bda13dfb1b93e
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-14 23:07:56 +1200

    AT3-151 working demo writes square wave to DADA file that is successfully folded using dspsr

--- matlab/SquareWave.m
+++ matlab/SquareWave.m
@@ -1,49 +1,55 @@
 classdef SquareWave
-   % generates amplitude-modulated noise
-   properties
-      period = 26        % samples
-      duty_cycle = 0.5   % duty cycle of the square wave
-      on_amp = 1.0       % standard deviation of on-pulse noise
-      off_amp = 0.0      % standard deviation of off-pulse noise
-      current = 0        % current sample
-   end
-   methods
-      function [obj, x] = generate (obj, nsample)
-      % returns:
-      %   obj = the modified object
-      %   x   = the next nsample samples of the wave
-      arguments
-         obj     (1,1) SquareWave
-         nsample (1,1) {mustBeInteger, mustBeNonnegative}
-      end
-         ioff = floor (obj.period*obj.duty_cycle);
-         nout = 0;
-         x = [];
-         while (nout < nsample)
-           
-           iphase = mod (obj.current, obj.period);
+    % generates amplitude-modulated noise
+    
+    properties
+        period = 26        % number of samples in one cycle of wave
+        duty_cycle = 0.5   % duty cycle of the square wave
+        on_amp = 1.0       % standard deviation of on-pulse noise
+        off_amp = 0.1      % standard deviation of off-pulse noise
+        current = 0        % current sample
+    end
+   
+    methods
+      
+        function [obj, x] = generate (obj, nsample)
+            % returns:
+            %   obj = the modified object
+            %   x   = the next nsample samples of the wave
 
-           if (iphase < ioff)
-               % on-pulse
-               n = ioff-iphase;
-               a = obj.on_amp;
-           else
-               % off-pulse
-               n = obj.period - iphase;
-               a = obj.off_amp;
-           end
+            arguments
+                obj     (1,1) SquareWave
+                nsample (1,1) {mustBeInteger, mustBeNonnegative}
+            end
+            
+            ioff = floor (obj.period*obj.duty_cycle);
+            nout = 0;
+            x = single([1,1,nsample]);
+            
+            while (nout < nsample)
+                
+                iphase = mod (obj.current, obj.period);
+                
+                if (iphase < ioff)
+                    % on-pulse
+                    n = ioff-iphase;
+                    a = obj.on_amp;
+                else
+                    % off-pulse
+                    n = obj.period - iphase;
+                    a = obj.off_amp;
+                end
+                
+                if (nout + n >= nsample)
+                    n = nsample - nout;
+                end
+                
+                % add n more random values to the output x
+                x(1,1,nout+(1:n)) = a*randn([1 n], 'single');
            
-           if (nout + n >= nsample)
-               n = nsample - nout;
-           end
-           
-           % add n more random values to the output x
-           x = [ x a*randn([1 n]) ];
-           
-           nout = nout + n;
-           obj.current = obj.current + n;
-           
-         end
-      end
-   end
-end
+                nout = nout + n;
+                obj.current = obj.current + n;
+                
+            end % of loop over remaining samples
+        end % of generate function
+    end % of methods section
+end % of SquareWave class definition
--- matlab/square_wave.m
+++ matlab/square_wave.m
@@ -1,10 +1,31 @@
 
-a = SquareWave
+sqwv = SquareWave;
 
-[a, x] = generate(a, 9);
-x
-[a, x] = generate(a, 9);
-x
-[a, x] = generate(a, 9);
-x
+file = DADAFile;
 
+file.filename = 'square_wave.dada';
+file.header_template = '../config/square_wave_header.json';
+
+json_str = fileread(file.header_template);
+header = struct2map(jsondecode(json_str));
+
+calfreq = str2num(header('CALFREQ'));  % in Hz
+tsamp = str2num(header('TSAMP'));      % in microseconds
+
+sqwv.period = 1e6 / (calfreq * tsamp); % in samples
+
+fprintf ('square_wave: frequency=%f Hz\n', calfreq);
+fprintf ('square_wave: sampling interval=%f microseconds\n', tsamp);
+fprintf ('square_wave: period=%d samples\n', sqwv.period);
+
+blocksz = 1024 * 1024;  % Mega sample in RAM
+%blocks = 1024;          % Giga sample to disk
+blocks = 2;
+
+for i = 1:blocks
+    fprintf ('block:%d\n', i);
+    [sqwv, x] = generate(sqwv, blocksz);
+    file = write (file, x);
+end
+
+file = close (file);
