d8113eb8440e69ea816a060db028cdbc646b275e
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-21 13:18:12 +1200

    AT3-152 adjust off-pulse power to work around limited dynamic range in psrfits files

--- matlab/AT3_152.sh
+++ matlab/AT3_152.sh
@@ -1,12 +1,14 @@
-#!/bin/csh
+#!/bin/bash
+
+echo DYLD=$DYLD_LIBRARY_PATH
 
 cd ../products
 
-foreach test ( "low" "low_psi" "low_alt" "low_psi_alt" "mid" )
+for test in "low" "low_psi" "low_alt" "low_psi_alt" "mid"; do
 
   echo test
   dspsr square_wave_${test}.dada -IF 1:1024:128 -dr -O square_wave_${test}
-  psrplot -pD -c log=1 square_wave_${test}.ar -D square_wave_${test}.eps/cps
+  psrplot -pD -c log=-1 square_wave_${test}.ar -D square_wave_${test}.eps/cps
 
-end
+done
 
