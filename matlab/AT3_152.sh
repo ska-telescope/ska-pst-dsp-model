bd65cd5e04141ada5575ca86d82fd64827e43f9f
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-26 14:18:19 +1200

    AT3-152 do not remove baseline before plotting on absolute log scale

--- matlab/AT3_152.sh
+++ matlab/AT3_152.sh
@@ -6,9 +6,11 @@ cd ../products
 
 for test in "low" "low_psi" "low_alt" "low_psi_alt" "mid"; do
 
-  echo test
+  echo Processing $test
   dspsr square_wave_${test}.dada -IF 1:1024:128 -dr -O square_wave_${test}
-  psrplot -pD -c log=-1 square_wave_${test}.ar -D square_wave_${test}.eps/cps
+  psrplot -x -pD -c log=-1 square_wave_${test}.ar -D square_wave_${test}.eps/cps
 
 done
+
+cd -
 
