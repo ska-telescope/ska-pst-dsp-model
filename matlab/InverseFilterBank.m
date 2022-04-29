3c8a3c2157b72f1c4c7f7da39311f5ee35d2de2a
Willem van Straten <vanstraten.willem@gmail.com>
2022-04-29 12:39:32 +1200

    AT3-153 ensure that number of input samples buffered is a multiple of the denominator of the oversampling factor

--- matlab/InverseFilterBank.m
+++ matlab/InverseFilterBank.m
@@ -70,13 +70,34 @@ classdef InverseFilterBank
                 obj.sample_offset+1, obj.overlap,...
                 obj.window_function,obj.conjugate_result,0);
             
-            output_size = size(output);
-            input_idat = output_size(3) * obj.os_factor.nu / (n_chan * obj.os_factor.de);
-            obj.buffered_samples = n_dat - input_idat;
+            remainder = 1;
+            while (remainder ~= 0)
+                output_size = size(output)
+                input_idat = output_size(3) * obj.os_factor.nu / (n_chan * obj.os_factor.de);
+                obj.buffered_samples = n_dat - input_idat;
+                            
+                remainder = mod (obj.buffered_samples, obj.os_factor.nu);
+                
+                if (remainder ~= 0)
+                    fprintf ('InverseFilterBank: buffer length = %d samples is not a multiple of %d\n',...
+                          obj.buffered_samples, obj.os_factor.nu);
+                    obj.buffered_samples = obj.buffered_samples + obj.os_factor.nu - remainder;
+                    input_idat = n_dat - obj.buffered_samples;
+                    output_ndat = input_idat * (n_chan * obj.os_factor.de) / obj.os_factor.nu;
+                    fprintf ('FilterBank: reducing output from %d to %d\n', output_size(3),output_ndat);
+                    output = output(:,:,1:output_ndat);
+                end
+            end
+
+            remainder = mod (obj.buffered_samples, obj.os_factor.nu);                
+            if (remainder ~= 0)
+                fprintf ('InverseFilterBank: buffer length = %d samples is not a multiple of %d\n',...
+                         obj.buffered_samples, obj.os_factor.nu);
+            end
             
             if (obj.buffered_samples > 0)
                 % fprintf ('Buffering %d samples\n', obj.buffered_samples);
-                obj.input_buffer = input (:,:,input_idat+1:end);
+                obj.input_buffer = input (:,:,(input_idat+1):end);
             end
             
         end % of execute function
