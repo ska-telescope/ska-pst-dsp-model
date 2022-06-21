2a5c254f53491d89dc31099826ba0b71e71144a8
Willem van Straten <vanstraten.willem@gmail.com>
2022-06-21 21:06:02 +1200

    AT3-216 output only the critically-sampled portion of the stage-one channels

--- matlab/TwoStageFilterBank.m
+++ matlab/TwoStageFilterBank.m
@@ -4,6 +4,7 @@ classdef TwoStageFilterBank < Channelizer
     properties
         stage1  (1,1) FilterBank
         stage2  (:,1) FilterBank
+        single = 0
     end
    
     methods
@@ -14,7 +15,7 @@ classdef TwoStageFilterBank < Channelizer
                  
             obj = obj@Channelizer;
             
-            if  nargin > 0
+            if nargin > 0
             
               fprintf ('TwoStageFilterBank::configure analysis function=%s\n',...
                      config.analysis_function);
@@ -24,6 +25,8 @@ classdef TwoStageFilterBank < Channelizer
                  obj.stage2(ichan) = FilterBank (config);
               end
             end
+            
+            obj.single = 1;
             
         end % of TwoStageFilterBank constructor
 
@@ -39,21 +42,25 @@ classdef TwoStageFilterBank < Channelizer
 
             fprintf ('stage 1\n');
             [obj.stage1, out1] = obj.stage1.execute (input);
-            nch = obj.stage1.n_chan;
             
-            fprintf ('stage 2\n');
-            for ich = 1:nch
+            os = obj.stage1.os_factor;
+            
+            nch1 = obj.stage1.n_chan;
+            nch2 = (nch1 * os.de) / os.nu;
+            offset = (nch1 - nch2) / 2;
+            
+            fprintf ('stage 2 nch2=%d offset=%d\n',nch2,offset);
+            for ich = 1:nch1
                 
                  [obj.stage2(ich), tmp] = obj.stage2(ich).execute (out1(1,ich,:));
                  
                  if (ich == 1)
-                     %% initialise
                      sz = size(tmp);
                      ndat = sz(3);
-                     out = zeros(1,nch*nch,ndat); 
+                     out = zeros(1,nch1*nch2,ndat); 
                  end
                  
-                 out(1,(1:nch)+((ich-1)*nch),:) = tmp;
+                 out(1,(1:nch2)+(ich-1)*nch2,:) = tmp(1,(1:nch2)+offset,:);
                  
             end
               
