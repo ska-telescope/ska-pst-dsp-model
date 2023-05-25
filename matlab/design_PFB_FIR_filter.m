fe93eecdd1fd00a578880c613211291e45814489
Willem van Straten <vanstraten.willem@gmail.com>
2023-05-25 15:21:39 +1200

    specify ntaps per channel instead of total number of taps

--- matlab/design_PFB_FIR_filter.m
+++ matlab/design_PFB_FIR_filter.m
@@ -1,11 +1,11 @@
-function fir_filter_path = design_PFB_FIR_filter(n_chan, os_factor, n_taps, display_)
+function fir_filter_path = design_PFB_FIR_filter(n_chan, os_factor, n_taps_per_chan, display_)
   % Design a FIR appropriate for polyphase filterbank.
   %
   % Example:
   %
   % .. code-block::
   %
-  %   >> design_PFB_FIR_filter(256, {'nu': 4, 'de': 3}, 256*10, 0);
+  %   >> design_PFB_FIR_filter(256, {'nu': 4, 'de': 3}, 12, 0);
   %
   % Args:
   %   n_chan (numeric): number of PFB output channels
@@ -42,6 +42,7 @@ function fir_filter_path = design_PFB_FIR_filter(n_chan, os_factor, n_taps, disp
   fprintf('design_PFB: stop-band frequency: %f\n', Fs);
 
   % Design filter
+  n_taps = n_chan * n_taps_per_chan;
   Hf = fdesign.lowpass('N,Fp,Fst',n_taps,Fp,Fs);
   H_Obj_0 = design(Hf,'firls','Wstop',15,'systemobject',true);
   % H_Obj_0 = design(Hf,'equiripple','Wstop',60,'StopbandShape','linear','StopbandDecay',30);
@@ -60,16 +61,11 @@ function fir_filter_path = design_PFB_FIR_filter(n_chan, os_factor, n_taps, disp
 %
 %     % Optionally display design
 
-
   if (display==1)
-      
     fprintf ('plotting response\n');
-      
     fig = plot_FIR_filter (n_chan, OS, h);
-
     saveas(fig, sprintf('./../products/FIR_filter_response.%d.png', n_taps));
-
-  end;
+  end
 
 %     close all;
 %
