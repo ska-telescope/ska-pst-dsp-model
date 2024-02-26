fcf61508586d975962b8afb82f37ff73d4e03f2f
Willem van Straten <vanstraten.willem@gmail.com>
2024-02-27 12:43:22 +1300

    AT3-565 compensate for underestimation of on-pulse variance and test 12-bit optimal rounding

--- at3_565_round_pfb_io.sh
+++ at3_565_round_pfb_io.sh
@@ -15,7 +15,7 @@ dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
 psrplot -pD -x -c log=-1 -jF ${out}.ar -D ${out}.png/png
 
 # scaling for optimal 8-bit rounded input
-file='products/square_wave_sps_lowpsi_two_stage_critical_rndIn_rmsIn=33.8.dada'
+file='products/square_wave_sps_lowpsi_two_stage_critical_rndIn_rmsIn=23.9002.dada'
 out=sw-low-8bitIn-2s-critical-IF-hanning
 
 dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
@@ -29,22 +29,22 @@ dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
 psrplot -pD -x -c log=-1 -jF ${out}.ar -D ${out}.png/png
 
 # scaling for optimal 8-bit rounded output
-file='products/square_wave_sps_lowpsi_two_stage_critical_rndOut_rmsOut=33.8.dada'
+file='products/square_wave_sps_lowpsi_two_stage_critical_rndOut_rmsOut=23.9002.dada'
 out=sw-low-8bitOut-2s-critical-IF-hanning
 
 dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
 psrplot -pD -x -c log=-1 -jF ${out}.ar -D ${out}.png/png
 
-# scaling for optimal 16-bit rounded output
-file='products/square_wave_sps_lowpsi_two_stage_critical_rndOut_rmsOut=3538.5.dada'
-out=sw-low-16bitOut-2s-critical-IF-hanning
+# scaling for optimal 12-bit rounded output
+file='products/square_wave_sps_lowpsi_two_stage_critical_rndOut_rmsOut=462.6.dada'
+out=sw-low-12bitOut-2s-critical-IF-hanning
 
 dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
 psrplot -pD -x -c log=-1 -jF ${out}.ar -D ${out}.png/png
 
-# scaling for optimal 8-bit rounded input, and scaling for optimal 16-bit rounded output
-file='products/square_wave_sps_lowpsi_two_stage_critical_rndIn_rmsIn=33.8_rndOut_rmsOut=3538.5.dada'
-out=sw-low-8bitIn-16bitOut-2s-critical-IF-hanning
+# scaling for optimal 16-bit rounded output
+file='products/square_wave_sps_lowpsi_two_stage_critical_rndOut_rmsOut=1769.25.dada'
+out=sw-low-16bitOut-2s-critical-IF-hanning
 
 dspsr $file -a PSRFITS -IF 256:1024:128 -O $out -f-taper=hanning
 psrplot -pD -x -c log=-1 -jF ${out}.ar -D ${out}.png/png
--- matlab/at3_565_round_pfb_io.m
+++ matlab/at3_565_round_pfb_io.m
@@ -1,7 +1,20 @@
 
 % see https://docs.google.com/spreadsheets/d/1F01T1KAoSTZOaW33wYVq6EZJk_xuwu3xuV2oohFhBrA/edit?usp=sharing
 optimal_8bit_rms = 33.8;
+optimal_12bit_rms = 462.6;
 optimal_16bit_rms = 3538.5;
+
+% Owing to the 50% duty cycle of the square wave, the on-pulse standard deviation 
+% is underestimated when the variance is computed over all samples.
+% The on-pulse variance is 2 times larger than the estimate; therefore,
+% the data should be scaled by
+%   s = rmsInput/onpulse_rms = rmsInput / (estimated_rms * sqrt(2))
+% This change is effected by using rmsInput' = rmsInput / sqrt(2)
+
+duty_cycle_correction = 1.0 / sqrt(2.0);
+optimal_8bit_rms = optimal_8bit_rms * duty_cycle_correction;
+optimal_16bit_rms = optimal_16bit_rms * duty_cycle_correction;
+optimal_16bit_rms = optimal_16bit_rms * duty_cycle_correction;
 
 % no rounding
 sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true);
@@ -18,8 +31,9 @@ sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true, rndOutput=
 % scaling for optimal 8-bit rounded output
 sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true, rmsOutput=optimal_8bit_rms);
 
+% scaling for optimal 12-bit rounded output
+sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true, rmsOutput=optimal_12bit_rms);
+
 % scaling for optimal 16-bit rounded output
 sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true, rmsOutput=optimal_16bit_rms);
 
-% scaling for optimal 8-bit rounded input, and scaling for optimal 16-bit rounded output
-sgcht (signal='square_wave', cfg='sps', cfg2='lowpsi', critical=true, rmsInput=optimal_8bit_rms, rmsOutput=optimal_16bit_rms);
