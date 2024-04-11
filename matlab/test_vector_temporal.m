4916927f0138310b4af85c7f3d0e2d12f20449cd
Willem van Straten <vanstraten.willem@gmail.com>
2024-04-11 21:45:30 +1200

    AT3-681 fleshing out the temporal test vector generator

--- matlab/test_vector_temporal.m
+++ matlab/test_vector_temporal.m
@@ -13,14 +13,38 @@ p = inputParser;
 % the correlator beam former for which the test vector is designed
 addOptional(p, 'cbf', 'low', @ischar);
 
+% number of test states
+addOptional(p, 'Nstate', 2, @isnumeric);
+
 % forward FFT length used during PFB inversion
 addOptional(p, 'Nfft', 0, @isnumeric);
 
-% amples by which forward FFTs overlap
+% samples by which forward FFTs overlap
 addOptional(p, 'Tover', 0, @isnumeric);
+
+% load header from file
+addOptional(p, 'header', '../config/test_vector_header.json', @ischar);
+
+% write DADA to file
+addOptional(p, 'output', '../products/test_vector_temporal.dada', @ischar);
 
 parse(p, varargin{:});
 
+header_file = p.Results.header;
+if ( header_file == "" )
+  error ('Missing header = name of JSON-formatted DADA header');
+end
+
+output_file = p.Results.output;
+if ( output_file == "" )
+  error ('Missing output = name of DADA-format output file');
+end
+
+Nstate = p.Results.Nstate;
+
+if ( Nstate < 1 )
+  error ('Invalid Nstate=%i',Nstate)
+end
 cbf = p.Results.cbf;
 
 if ( cbf == "low" )
@@ -43,42 +67,31 @@ end
 
 if ( p.Results.Nfft > 0 )
     Nfft = p.Results.Nfft;
-    print('Setting Nfft to %i',Nfft)
-end
-
-if ( header_file == "" )
-  error ('Missing header=name of JSON formatted DADA header');
-end
-
-if ( output_file == "" )
-  error ('Missing output=name of DADA format output file');
+    fprintf('Setting Nfft to %i',Nfft)
 end
 
 fileID = fopen (header_file, 'r');
 header = read_header(fileID);
 fclose(fileID);
 
-[fb_data] = load_fb_tb_data(input_file, 4, 3);
+fprintf('header parsed from JSON string');
 
-fb_size = size(fb_data)
-% (20,:,1,1) = fine frequency 20, all time samples, first polarisation, 
-% first virtual channel (which is configured to have a delay of 0 in the simulation)
-
-n_pol=1
-n_chan=fb_size(1);
-n_samp=fb_size(2);
-
-x = complex(zeros(n_pol, n_chan, n_samp));
-x(1,:,:) = fb_data(:,:,1,virtual_channel);
-
-stddev = sqrt(var(x,0,"all"));
-fprintf ("input rms=%e \n", stddev);
-
-data = complex(cast(x,"single"));
+ndat = Nfft - Tover;
+data = complex(cast(zeros(ndat),"single"));
 
 fileID = fopen (output_file, 'w');
 write_dada_header (fileID, data, header);
-write_dada_data (fileID, data);
+
+fprintf('header written to DADA file')
+
+fprintf('writing %i blocks of %i samples',Nstate, ndat)
+for istate = 1:Nstate
+
+    data = complex(cast(zeros(ndat),"single"));
+    write_dada_data (fileID, data);
+
+end
+
 fclose(fileID);
 
 result = 0;
